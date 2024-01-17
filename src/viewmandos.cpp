2a0826ef6759d9f9910dad5d2900189d8efa44de
Magí Romanyà <magiromanya@gmail.com>
2024-01-17 13:40:47 +0100

    Fix on windows

--- src/viewmandos.cpp
+++ src/viewmandos.cpp
@@ -20,9 +20,9 @@
 
 Camera3D create_camera(unsigned int FPS = 200) {
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 5.0f, 20.0f };  // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.position = Vector3( 0.0f, 5.0f, 20.0f );  // Camera position
+    camera.target = Vector3( 0.0f, 0.0f, 0.0f );      // Camera looking at point
+    camera.up = Vector3( 0.0f, 1.0f, 0.0f );          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type    return camera;
 
