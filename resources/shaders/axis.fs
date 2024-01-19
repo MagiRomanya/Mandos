2b11aa95eecd0a6408475d962422876ccf82203f
Magí Romanyà <magiromanya@gmail.com>
2024-01-19 22:40:18 +0100

    Better axis shader :)

--- resources/shaders/axis.fs
+++ resources/shaders/axis.fs
@@ -14,6 +14,21 @@ uniform vec4 colDiffuse;
 // Output fragment color
 out vec4 finalColor;
 
+#define     LIGHT_DIRECTIONAL       0
+#define     LIGHT_POINT             1
+
+struct Light {
+    int enabled;
+    int type;
+    vec3 position;
+    vec3 target;
+    vec4 color;
+};
+
+// Input lighting values
+vec4 ambient = vec4(0.2);
+vec3 viewPos = vec3(0.0, 0.0, 1.0);
+
 void main()
 {
     vec4 modelColor;
@@ -35,5 +50,46 @@ void main()
     else {
         modelColor = colDiffuse;
     }
-    finalColor = modelColor;
+
+    // Texel color fetching from texture sampler
+    vec4 texelColor = texture(texture0, fragTexCoord);
+    vec3 lightDot = vec3(0.0);
+    vec3 normal = normalize(fragNormal);
+    vec3 viewD = normalize(viewPos - fragPosition);
+    vec3 specular = vec3(0.0);
+
+    Light light;
+
+    light.enabled = 1;
+    light.type = LIGHT_DIRECTIONAL;
+    light.position = vec3(0, 0, 4);
+    light.target = vec3(0, 0, 0);
+    light.color = vec4(1); // white light
+
+    if (light.enabled == 1){
+      vec3 light_vec = vec3(0.0);
+
+      if (light.type == LIGHT_DIRECTIONAL)
+      {
+        light_vec = normalize(light.position - light.target);
+      }
+
+      if (light.type == LIGHT_POINT)
+      {
+        light_vec = normalize(light.position - fragPosition);
+      }
+
+      float NdotL = max(dot(normal, light_vec), 0.0);
+      lightDot += light.color.rgb * NdotL;
+
+      float specCo = 0.0;
+      if (NdotL > 0.0) specCo = pow(max(0.0, dot(viewD, reflect(-(light_vec), normal))), 16.0); // 16 refers to shine
+      specular += specCo;
+    }
+
+    finalColor = (texelColor*((modelColor + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
+    finalColor += texelColor*(ambient)*modelColor;
+
+    // Gamma correction
+    finalColor = pow(finalColor, vec4(1.0/2.2));
 }
